dd5a42a7e07ff190d6ea431dcdaf4c58bef2eaff
lucianojd <lucdegianni@gmail.com>
2025-05-27 21:34:12 -0700

    chore: cleaned repository

--- README.md
+++
@@ -1,4 +0,0 @@
-# MyWebsite
-
-MyWebsite is meant to be an extension of my resume. It contains pages discussing my
-interests, skills, education and projects.
--- components/common/box.tsx
+++
@@ -1,9 +0,0 @@
-import styles from '@styles/components/box.module.scss';
-
-type BoxProps = JSX.IntrinsicElements['div'];
-
-function Box(props: BoxProps) {
-  return <div className={styles.container} {...props} />;
-}
-
-export default Box;
