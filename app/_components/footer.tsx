4eeadeacaa782a2b5af233b84a1ed137a89c4876
Luciano De Gianni <lucdegianni@gmail.com>
2025-12-05 15:13:39 -0800

    chore: added footer links

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -1,9 +1,19 @@
 import { JSX } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
 
 function Footer(): JSX.Element {
   return (
     <footer>
-      <a href="/">lucianod.me</a>
+      <Link href="/">
+        <Image fill alt="lucianod.me" src={'/cubed_earth.png'} />
+      </Link>
+      <Link href="https://github.com/lucianojd">
+        <Image fill alt="GitHub" src={'/github.svg'} />
+      </Link>
+      <Link href="https://www.linkedin.com/in/lucianojd">
+        <Image fill alt="LinkedIn" src={'/linkedin.png'} />
+      </Link>
     </footer>
   );
 }
--- app/projects/projects-page.tsx
+++ app/projects/projects-page.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 export default function ProjectsPage() {
   return (
     <main>
@@ -5,6 +7,17 @@ export default function ProjectsPage() {
       <p>
         Welcome to the Projects Page. Here you will find a list of my projects.
       </p>
+      <h1>More about me</h1>
+      <ul>
+        <li>
+          <Link href="https://github.com/lucianojd">My GitHub profile</Link>
+        </li>
+        <li>
+          <Link href="https://www.linkedin.com/in/lucianojd/">
+            My LinkedIn profile
+          </Link>
+        </li>
+      </ul>
     </main>
   );
 }
